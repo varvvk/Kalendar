a9ba5cc1a61146f1aca947b719a7fdc7f22afdb6
Alex <mysli2007@gmail.com>
2023-12-24 21:41:11 +0300

    финальная версия

--- Kalendar/module1.cpp
+++ Kalendar/module1.cpp
@@ -4,6 +4,17 @@
 #include <ctime>
 
 int main() {
+	setlocale(LC_ALL, "Russian");
+
+	time_t seconds = time(nullptr);
+	tm* timeinfo = localtime(&seconds);
+
+	char buffer[80];
+	strftime(buffer, sizeof(buffer), "%A, %d-%m-%Y. Порядковый номер дня в году: %j", timeinfo);
+
+	std::cout << "Сегодняшняя дата: " << buffer << std::endl;
+
+	std::cin.get();
 	return 0;
 
 }
