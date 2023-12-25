cf5c3786e003e1d05e33a6bb726a2acd7e7c06ed
Vika <154736446+vitaaZ@users.noreply.github.com>
2023-12-25 15:56:08 +0300

    Added untilNewYear.cpp and case2.cpp with .h

---
+++ untilNewYear.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include "case2.h"
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+
+	int n;
+	cin >> n;
+	switch (n)
+	{
+	case 2:
+		case2();
+		break;
+	default:
+		break;
+	}
+}
