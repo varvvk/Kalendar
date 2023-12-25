4035748f611828e6715f2056c1a86db801cfd826
Vika <154736446+vitaaZ@users.noreply.github.com>
2023-12-25 18:18:33 +0300

    Added furTree.cpp and furTree.h

---
+++ furTree.cpp
@@ -0,0 +1,34 @@
+#include "furTree.h"
+#include <iomanip>
+
+int furTree() {
+	using namespace std;
+
+	char c = 3, probel = ' ';
+	cout << setw(18) << setfill(probel) << probel;
+	cout << setw(1) << setfill(c) << c << endl;
+	cout << setw(17) << setfill(probel) << probel;
+	cout << setw(3) << setfill(c) << c << endl;
+	cout << setw(16) << setfill(probel) << probel;
+	cout << setw(5) << setfill(c) << c << endl;
+	cout << setw(15) << setfill(probel) << probel;
+	cout << setw(7) << setfill(c) << c << endl;
+	cout << setw(16) << setfill(probel) << probel;
+	cout << setw(5) << setfill(c) << c << endl;
+	cout << setw(15) << setfill(probel) << probel;
+	cout << setw(7) << setfill(c) << c << endl;
+	cout << setw(14) << setfill(probel) << probel;
+	cout << setw(9) << setfill(c) << c << endl;
+	cout << setw(15) << setfill(probel) << probel;
+	cout << setw(7) << setfill(c) << c << endl;
+	cout << setw(14) << setfill(probel) << probel;
+	cout << setw(9) << setfill(c) << c << endl;
+	cout << setw(13) << setfill(probel) << probel;
+	cout << setw(11) << setfill(c) << c << endl;
+	cout << setw(17) << setfill(probel) << probel;
+	cout << setw(3) << setfill(c) << c << endl;
+	cout << setw(17) << setfill(probel) << probel;
+	cout << setw(3) << setfill(c) << c << endl;
+
+	return 0;
+}
