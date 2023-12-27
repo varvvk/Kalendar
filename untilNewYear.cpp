12af3b6df80b767380e86d7673be7aea1da7c31d
Alex <141527321+simplisticalex@users.noreply.github.com>
2023-12-27 18:06:40 +0300

    Update untilNewYear.cpp (added module3 in switch case)

--- untilNewYear.cpp
+++ untilNewYear.cpp
@@ -1,26 +1,46 @@
-﻿#include <iostream>
+#include <iostream>
 #include "case2.h"
 #include "case1.h"
+#include "case3.h"
 using namespace std;
 
 int main()
 {
-	setlocale(LC_ALL, "Russian");
-	system("mode con cols=35 lines=25");
+setlocale(LC_ALL, "Russian");
+system("mode con cols=35 lines=25");
 
-	int n;
-	cin >> n;
-	switch (n)
+int choice = 0;
+while (choice != 4) {
+	cout << "Выберите действие:" << endl;
+	cout << "1. Вывести дату, порядковый номер дня в году" << endl;
+	cout << "2. Вывести сколько дней осталось до Нового года" << endl;
+	cout << "3. Вывести новогоднюю цитату" << endl;
+	cout << "4. Выйти" << endl;
+	cin >> choice;
+
+	switch (choice)
 	{
 	case 1: 
 		system("cls");
+		cout << "Выполнено действие 1\n";
 		case1();
 		break;
 	case 2:
 		system("cls");
+		cout << "Выполнено действие 2\n";
 		case2();
+		break;
+	case 3:
+		system("cls");
+		cout << "Выполнено действие 3\n";
+		case3();
+		break;
+	case 4:
+		cout << "Выход из программы\n";
 		break;
 	default:
 		break;
 	}
+}
+return 0;
 }
