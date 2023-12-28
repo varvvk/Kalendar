e707e94f81ca7e008263612a23513ebb20fa8b9f
Alex <141527321+simplisticalex@users.noreply.github.com>
2023-12-28 03:48:34 +0300

    Update case3.cpp

--- case3.cpp
+++ case3.cpp
@@ -6,40 +6,40 @@ int case3() {
     using namespace std;
     tm newYearDate = {}; 
 
-    newYearDate.tm_year = 2024 - 1900; // 2024 год
+    newYearDate.tm_year = 2024 - 1900; // 2024 ГЈГ®Г¤
     newYearDate.tm_mon = 0;            
     newYearDate.tm_mday = 1;           
     newYearDate.tm_hour = 0;           
     newYearDate.tm_min = 0;           
     newYearDate.tm_sec = 0;           
 
-    chrono::system_clock::time_point nowTime = chrono::system_clock::now();  // Текущее время
-    chrono::system_clock::time_point eventTime = chrono::system_clock::from_time_t(mktime(&newYearDate));  // Время новогоднего момента
+    chrono::system_clock::time_point nowTime = chrono::system_clock::now();  // РўРµРєСѓС‰РµРµ РІСЂРµРјСЏ
+    chrono::system_clock::time_point eventTime = chrono::system_clock::from_time_t(mktime(&newYearDate));  // Р’СЂРµРјСЏ РЅРѕРІРѕРіРѕРґРЅРµРіРѕ РјРѕРјРµРЅС‚Р°
 
-    chrono::duration<int> timeDifference = chrono::duration_cast<chrono::duration<int>>(eventTime - nowTime);  // Вычисление разницы во времени
+    chrono::duration<int> timeDifference = chrono::duration_cast<chrono::duration<int>>(eventTime - nowTime);  // Р’С‹С‡РёСЃР»РµРЅРёРµ СЂР°Р·РЅРёС†С‹ РІРѕ РІСЂРµРјРµРЅРё
 
     int daysUntil = timeDifference.count() / (60 * 60 * 24);
     
     const char newYearQuotes[5][157] = {
-        "Пусть наступающий год будет наполнен светлыми моментами и приятными сюрпризами!",
-        "С наступающим Новым годом! Пусть он принесет удачу, здоровье и счастье!",
-        "Поздравляю с Новым годом! Пусть он будет наполнен яркими впечатлениями и удачей!",
-        "Счастливого Нового года! Пусть он подарит радость, тепло и вдохновение!",
-        "Новый год — время перемен и возможностей. Пусть он принесет удачу во всех начинаниях!"
-    };
+    "РџСѓСЃС‚СЊ РЅР°СЃС‚СѓРїР°СЋС‰РёР№ РіРѕРґ Р±СѓРґРµС‚ РЅР°РїРѕР»РЅРµРЅ СЃРІРµС‚Р»С‹РјРё РјРѕРјРµРЅС‚Р°РјРё Рё РїСЂРёСЏС‚РЅС‹РјРё СЃСЋСЂРїСЂРёР·Р°РјРё!",
+    "РЎ РЅР°СЃС‚СѓРїР°СЋС‰РёРј РќРѕРІС‹Рј РіРѕРґРѕРј! РџСѓСЃС‚СЊ РѕРЅ РїСЂРёРЅРµСЃРµС‚ СѓРґР°С‡Сѓ, Р·РґРѕСЂРѕРІСЊРµ Рё СЃС‡Р°СЃС‚СЊРµ!",
+    "РџРѕР·РґСЂР°РІР»СЏСЋ СЃ РќРѕРІС‹Рј РіРѕРґРѕРј! РџСѓСЃС‚СЊ РѕРЅ Р±СѓРґРµС‚ РЅР°РїРѕР»РЅРµРЅ СЏСЂРєРёРјРё РІРїРµС‡Р°С‚Р»РµРЅРёСЏРјРё Рё СѓРґР°С‡РµР№!",
+    "РЎС‡Р°СЃС‚Р»РёРІРѕРіРѕ РќРѕРІРѕРіРѕ РіРѕРґР°! РџСѓСЃС‚СЊ РѕРЅ РїРѕРґР°СЂРёС‚ СЂР°РґРѕСЃС‚СЊ, С‚РµРїР»Рѕ Рё РІРґРѕС…РЅРѕРІРµРЅРёРµ!",
+    "РќРѕРІС‹Р№ РіРѕРґ вЂ” РІСЂРµРјСЏ РїРµСЂРµРјРµРЅ Рё РІРѕР·РјРѕР¶РЅРѕСЃС‚РµР№. РџСѓСЃС‚СЊ РѕРЅ РїСЂРёРЅРµСЃРµС‚ СѓРґР°С‡Сѓ РІРѕ РІСЃРµС… РЅР°С‡РёРЅР°РЅРёСЏС…!"
+};    
 
 
-    std::cout << "Цитата на сегодня (" << daysUntil << " ";
+    std::cout << "Р¦РёС‚Р°С‚Р° РЅР° СЃРµРіРѕРґРЅСЏ (" << daysUntil << " ";
     if (daysUntil == 1) {
-        std::cout << "день";
+        std::cout << "РґРµРЅСЊ";
     }
     else if (daysUntil == 2 || daysUntil == 3 || daysUntil == 4) {
-        std::cout << "дня";
+        std::cout << "РґРЅСЏ";
     }
     else {
-        std::cout << "дней";
+        std::cout << "РґРЅРµР№";
     }
-    std::cout << " до Нового года): " << std::endl;
+    std::cout << " РґРѕ РќРѕРІРѕРіРѕ РіРѕРґР°): " << std::endl;
     std::cout << newYearQuotes[daysUntil] << std::endl;
     return 0;
 }
